49d03774272bd96876c08203b29eece299496365
Mitchell Hummel <mustbemitchell@gmail.com>
2025-02-17 21:56:34 +0100

    add api documentation

--- src/glacier/__init__.py
+++ src/glacier/__init__.py
@@ -2,7 +2,7 @@ from glacier.base import ValidationBase
 from glacier.column import Column
 from glacier.exceptions import GlacierValidationException, GlacierCriticalException
 from glacier.settings import ValidationSettings
-from glacier.wrappers import validator
+from glacier.wrappers import validation_check
 
 __all__ = [
     "ValidationBase",
@@ -10,5 +10,5 @@ __all__ = [
     "GlacierValidationException",
     "GlacierCriticalException",
     "ValidationSettings",
-    "validator",
+    "validation_check",
 ]
--- src/glacier/wrappers.py
+++ src/glacier/wrappers.py
@@ -1,7 +1,7 @@
 from typing import Callable, Optional
 
 
-def validator(selection: Optional[list[str]] = None) -> Callable:
+def validation_check(selection: Optional[list[str]] = None) -> Callable:
     """
     Wraps a function that returns a validation expression. All wrapped functions
     are expected to receive at least 1 argument, which would be the column name.
@@ -29,33 +29,30 @@ def validator(selection: Optional[list[str]] = None) -> Callable:
 
     Examples
     --------
-    ```python
-    from polars import Expr, col, lit
-    from glacier import ValidationBase, Column, validator
+    Creating a validator class and defining validation rules:
 
-    class ExampleValidator(ValidationBase):
-        settings = ValidationSettings(strict=False)
-        id: int = Column(name="Index")
-        first_name: str = Column(name="First Name")
-        last_name: str = Column(name="Last Name")
-        address: str = Column(name="Address")
-        gross_income: float = Column(name="Gross-Income")
-
-        @validator(selection=["First Name", "Last Name"])
-        def check_is_alphabetic(column: str) -> tuple[Expr, str]:
-            # Anything that is not alphabetic needs to receive the error
-            expression = col(column).str.contains(pattern="/^[A-Za-z]+$/").not_()
-            error = f"{column} can only contain alphabetic characters!"
-
-            return expression, error
-
-        @validator(selection=["Gross-Income"])
-        def check_not_negative(column: str) -> tuple[Expr, str]:
-            expression = col(column).lt(lit(0.0))
-            error = f"{column} cannot be lower than 0!"
-
-            return expression, error
-    ```
+    >>> from polars import Expr, col, lit
+    >>> from glacier import ValidationBase, Column, validation_check, ValidationSettings
+    ...
+    >>> class ExampleValidator(ValidationBase):
+    ...     settings = ValidationSettings(strict=False)
+    ...     id: int = Column(name="Index")
+    ...     first_name: str = Column(name="First Name")
+    ...     last_name: str = Column(name="Last Name")
+    ...     address: str = Column(name="Address")
+    ...     gross_income: float = Column(name="Gross-Income")
+    ...
+    ...     @validation_check(selection=["First Name", "Last Name"])
+    ...     def check_is_alphabetic(column: str) -> Tuple[Expr, str]:
+    ...         expression = col(column).str.contains(pattern="/^[A-Za-z]+$/").not_()
+    ...         error = f"{column} can only contain alphabetic characters!"
+    ...         return expression, error
+    ...
+    ...     @validation_check(selection=["Gross-Income"])
+    ...     def check_not_negative(column: str) -> Tuple[Expr, str]:
+    ...         expression = col(column).lt(lit(0.0))
+    ...         error = f"{column} cannot be lower than 0!"
+    ...         return expression, error
     """
     type_error = "Argument 'selection' expects a list of strings representing the column names needed for executing the validation check!"
 
