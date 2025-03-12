d7eab18408df06f0868d2acc42f53427f85b1216
Mitchell Hummel <mustbemitchell@gmail.com>
2025-03-12 20:48:34 +0100

    add more string validations

--- src/glacier/validators/string.py
+++ src/glacier/validators/string.py
@@ -11,11 +11,20 @@ def check_min_length(column: str, length: int, index: int) -> Expr:
         .alias(f"__error_{index}")
     )
 
+def check_max_length(column: str, length: int, index: int) -> Expr:
+    return (
+        when(col(column).str.len_chars().gt(lit(length)))
+        .then(lit(f"{column} requires a maximal length of {length} characters."))
+        .alias(f"__error_{index}")
+    )
 
-def get_string_validators(column: str, min_length: Optional[int]) -> list[Expr]:
+def get_string_validators(column: str, min_length: Optional[int], max_length: Optional[int]) -> list[Expr]:
     validators = []
 
     if min_length:
         validators.append(partial(check_min_length, column=column))
+
+    if max_length:
+        validators.append(partial(check_max_length, column=column))
 
     return validators
