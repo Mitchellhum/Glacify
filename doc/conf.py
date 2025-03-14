4632a12c32f2ce766da507b1570c451f69d0f220
Mitchell Hummel <mustbemitchell@gmail.com>
2025-03-14 22:41:12 +0100

    build for new version

--- doc/conf.py
+++ doc/conf.py
@@ -19,7 +19,7 @@ sys.path.insert(0, os.path.abspath(path))
 project = 'Glacify'
 copyright = '2025, Mitchell Hummel'
 author = 'Mitchell Hummel'
-release = 'v0.1.0'
+release = 'v0.2.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
